0f7513da852d08635202780bee2c2983a381b6d3
Matt Roe <matt.roe@patientpoint.com>
2018-01-03 16:49:06 -0500

    Added svgs / carousel

---
+++ resources/views/carousel/carousel.blade.php
@@ -0,0 +1,3 @@
+<div class="carousel">
+  <h1 class="h1"><a href="{{ url('/category') }}">Welcome</a></h1>
+</div>
---
+++ resources/views/shared/svgs/menu.blade.php
@@ -0,0 +1,7 @@
+<svg version="1.1" class="svg svg--menu" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 94 94" width="" style="enable-background:new 0 0 94 94;" xml:space="preserve">
+	<circle fill="#28699a" stroke="#fff" stroke-width="4" cx="47" cy="47.5" r="43.5"></circle>
+	<line fill="none" stroke="#fff" stroke-width="3" x1="30" y1="30" x2="55" y2="30"></line>
+	<line fill="none" stroke="#fff" stroke-width="3" x1="30" y1="38" x2="63" y2="38"></line>
+	<line fill="none" stroke="#fff" stroke-width="3" x1="30" y1="46" x2="55" y2="46"></line>
+	<text transform="matrix(1.2 0 0 1.2 30 70)" fill="#fff">MENU</text>
+</svg>
